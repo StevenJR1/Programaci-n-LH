8c8859e7f6a919f01f32116684442d33cecad22d
StevenJR1 <106745470+StevenJR1@users.noreply.github.com>
2022-08-05 10:33:18 -0500

    Add files via upload

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            RellenarGrid();
+        }
+        private void RellenarGrid()
+        {
+            List<Atención_primaria> datos = new List<Atención_primaria>();
+            datos.Add(new Atención_primaria("Bloque 15", "Sep 15", "Nov 30", "Mañana"));
+            datos.Add(new Atención_primaria("Bloque 5", "Ener 5", "Marz 15", "Tarde"));
+
+            foreach (Atención_primaria d in datos)
+            {
+                int rowIndex = dataGridView1.Rows.Add();
+                DataGridViewRow row = dataGridView1.Rows[rowIndex];
+                row.Cells[0].Value = d.Column1;
+                row.Cells[1].Value = d.Column2;
+                row.Cells[2].Value = d.Column3;
+                row.Cells[3].Value = d.Column4;
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            AdjustDatagridview1Height();
+        }
+        private void AdjustDatagridview1Height()
+        {
+            var height = dataGridView1.ColumnHeadersHeight;
+            foreach (DataGridViewRow dr in dataGridView1.Rows)
+            {
+                height += dr.Height;
+            }
+            dataGridView1.Height = height;
+        }
+    }
+}
